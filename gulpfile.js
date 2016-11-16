d85523f6a5df248b1f3dfcdb313f5f95d6521c9b
Johan Gorter <johan-gorter@users.noreply.github.com>
2016-11-16 15:13:29 +0100

    bumps package version

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var path = require('path');
 var git = require('gulp-git');
 var bump = require('gulp-bump');
 var filter = require('gulp-filter');
+var tag_version = require('gulp-tag-version');
 
 var ts = require('gulp-typescript');
 var plumber = require('gulp-plumber');
@@ -83,15 +84,13 @@ gulp.task('declaration', function() {
 
 function inc(importance) {
   // get all the files to bump version in
-  return gulp.src(['./package.json', './bower.json'])
+  return gulp.src(['./package.json'])
     // bump the version number in those files
     .pipe(bump({ type: importance }))
     // save it back to filesystem
     .pipe(gulp.dest('./'))
     // commit the changed version number
     .pipe(git.commit('bumps package version'))
-    // read only one file to get the version number
-    .pipe(filter('package.json'))
     // **tag it in the repository**
     .pipe(tag_version());
 }
